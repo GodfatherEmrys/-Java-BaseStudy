e45f0a9904b1149fbf6f1e9bcc7a1b8d93ada293
Arth <gift8128@naver.com>
2017-10-31 21:03:24 +0900

    Java Algorithm Study
    1. Recursive function
    2. Fibonacci Sequence.

---
+++ src/study/algorithm/recursive/FibonacciSequence.java
@@ -0,0 +1,55 @@
+package study.algorithm.recursive;
+
+import java.util.Scanner;
+
+/**
+ * @author Emrys.
+ * Algorithm Study.
+ * @Recursivefunction.
+ * @FibonacciSequence.
+ */
+public class FibonacciSequence {
+	
+	/**
+	 * main.
+	 * @param args.
+	 * @input: (Integer).
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input number: ");
+		try {
+			int inputNumber = sc.nextInt();
+			fibonacciPrint(inputNumber);
+		}catch(NumberFormatException e) {
+			e.printStackTrace();
+		}
+		sc.close();
+	}
+	
+	/**
+	 * @function() fibonacci sequence by recursive function.
+	 * @param (Integer)inputNumber.
+	 * @return (Integer).
+	 */
+	private static int fibonacci(int inputNumber) {
+		if(inputNumber < 2) return inputNumber;
+		return fibonacci(inputNumber-1) + fibonacci(inputNumber-2);
+	}
+	
+	/**
+	 * @function() fibonacci sequence Printing method.
+	 * @param (Integer)number.
+	 * @return : Recursion termination == (forPrintNumber == inputNumber)
+	 */
+	static int forPrintNumber = 1;
+	private static void fibonacciPrint(int inputNumber) {
+		if(forPrintNumber == inputNumber) {
+			System.out.print("[" + forPrintNumber + "]" + fibonacci(forPrintNumber));
+			return;
+		}
+		System.out.print("[" + forPrintNumber + "]" + fibonacci(forPrintNumber) + " => ");
+		forPrintNumber++;
+		fibonacciPrint(inputNumber);
+	}
+}

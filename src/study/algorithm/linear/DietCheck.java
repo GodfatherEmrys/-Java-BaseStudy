546df00db2432284e836358d591979722faa79d3
Arth <gift8128@naver.com>
2017-11-01 13:57:08 +0900

    Update Time complexity & better way algoritm.

--- src/study/algorithm/linear/DietCheck.java
+++ src/study/algorithm/linear/DietCheck.java
@@ -3,18 +3,23 @@
  * @Reference: Algorithm Problem Solving Strategies.
  * @MovingAverage
  */
+
 package study.algorithm.linear;
 
 import java.util.ArrayList;
+
 import java.util.Scanner;
+
 /**
  * @author Emrys.
- * Linear function,
- * Moving Average,
+ * #Linear function,
+ * #Moving Average,
+ * #Time Complexity,
  */
 public class DietCheck {
 	
 	/**
+	 * Test main function().
 	 * @param args
 	 */
 	public static void main(String[]args) {
@@ -26,6 +31,7 @@ public class DietCheck {
 		
 		System.out.print("Input the count of diet records: ");
 		int checkDays = sc.nextInt();
+		
 		for(int i = 0; i < checkDays; i++) {
 			dietList.add(sc.nextDouble());
 		}
@@ -33,7 +39,7 @@ public class DietCheck {
 		System.out.print("Input the MA[Moving Average] check Length: ");
 		int checkMALength = sc.nextInt();
 		
-		movingAverageList  = movingAverageOne(dietList, checkMALength);
+		movingAverageList  = movingAverageSecondWay(dietList, checkMALength);
 		
 		for(int i = 0; i < movingAverageList.size(); i++) {
 			System.out.println(movingAverageList.get(i));
@@ -43,15 +49,14 @@ public class DietCheck {
 	}
 	
 	/**
-	 * 
+	 * This is simple way to get result, however, if it can not process for diet list of huge size.
+	 * { N - (M - 1) } * N = N^2 - M*N + N = O(N^2)
 	 * @param dietList			the list of diet records.
 	 * @param checkMALength		Moving Average Length for checking.
 	 * @return					the list of Moving Average. (size() == dietList.size - checkMALength)
 	 */
-	public static ArrayList<Double> movingAverageOne(ArrayList<Double> dietList, int checkMALength){
-		/*
-		if(dietList.size() < checkMALength) throw new ArrayIndexOutOfBoundsException("MA check Length can not over diet records list size.");
-		*/
+	public static ArrayList<Double> movingAverageFirstWay(ArrayList<Double> dietList, int checkMALength){
+		
 		ArrayList<Double> result = new ArrayList<Double>();
 		int wholeDays = dietList.size();	
 		
@@ -59,6 +64,30 @@ public class DietCheck {
 			double partialSum = 0;
 			for(int j = 0; j < checkMALength; ++j) partialSum += dietList.get(i-j);
 			result.add(partialSum / checkMALength);
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * This way can remove duplicate calculation.
+	 * M - 1 + { N - ( M - 1 ) } = O(N)
+	 * @param dietList			the list of diet records.
+	 * @param checkMALength		Moving Average Length for checking.
+	 * @return					the list of Moving Average. (size() == dietList.size - checkMALength)
+	 */
+	public static ArrayList<Double> movingAverageSecondWay(ArrayList<Double> dietList, int checkMALength){
+		
+		ArrayList<Double> result = new ArrayList<Double>();
+		int wholeDays = dietList.size();
+		double partialSum = 0;
+		
+		for(int i=0; i < checkMALength - 1; i++) partialSum += dietList.get(i);
+		
+		for(int i = checkMALength - 1; i < wholeDays; ++i) {
+			partialSum += dietList.get(i);
+			result.add(partialSum / checkMALength);
+			partialSum -= dietList.get(i - checkMALength + 1);
 		}
 		
 		return result;

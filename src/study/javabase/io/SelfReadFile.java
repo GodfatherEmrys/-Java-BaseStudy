ed64c8191cd00ce1dc0f9a888105c9ed238cff8e
Arth <gift8128@naver.com>
2017-11-03 17:09:15 +0900

    Update Self Read Class

--- src/study/javabase/io/SelfReadFile.java
+++ src/study/javabase/io/SelfReadFile.java
@@ -1,11 +1,23 @@
+/**
+ * Study Information
+ * @Reference: JAVA Programming Architecture.
+ * @Content: File System.
+ */
+
 package study.javabase.io;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * This sample class is Self Read Class by JAVA File System.
+ * @author Emrys.
+ *
+ */
 public class SelfReadFile {
 
 	public static void main(String[]args) {
@@ -20,8 +32,11 @@ public class SelfReadFile {
 				while((str = br.readLine()) != null) {
 					System.out.println(str);
 				}
-			} catch (Exception e) {
+			} catch (IOException e) {
 				e.printStackTrace();
+			} finally {
+				try { if (inputStreamReader != null) inputStreamReader.close();} catch ( IOException io) { io.printStackTrace(); }
+				try { if (br != null) br.close();} catch ( IOException io) { io.printStackTrace(); }
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();

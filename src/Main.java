ac303fd5f9f44730805772d34d681caf4186c58f
Arth <gift8128@naver.com>
2017-11-05 21:58:41 +0900

    Organized

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		try {
-			System.out.print(Calculator.Calculate("1+1"));	
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
 		sc.close();	
 	}
 }

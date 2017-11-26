d640f2159d37194084446b92948866a92697ff45
Arth <gift8128@naver.com>
2017-11-26 20:17:53 +0900

    recursive explain

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
-import java.util.*;
 
 public class Main {
 	
+	public int recursive(int number) {
+		/* Base Case */
+		/* 종료 조건 */
+		if(number == 0) return 0;
+		if(number == 1) return 1;
+		
+		/* 어떤 과정으로 할 것인가 : 점화식, 패턴 */
+		return recursive(number-1) + recursive(number-2);
+	}
+	
 	
 	public static void main(String[] args) {
-		Main rs = new Main();
+		
 		
 		
 	}
